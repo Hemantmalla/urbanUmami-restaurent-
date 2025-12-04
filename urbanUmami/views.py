dcc943e03c6098370d7349dd414d5e69fec3850d
Hemanthmalla <hemanthmalla2131@gmail.com>
2025-12-04 22:52:00 +0530

    Initializing the repo

---
+++ urbanUmami/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+def about(request):
+    return render(request, 'urbanUmami/about.html')
+
+def home(request):
+    return render(request, 'urbanUmami/home.html')
+
+def index(request):
+    return render(request, 'urbanUmami/index.html')
+
+def menu(request):
+    return render(request, 'urbanUmami/menu.html')
+
+def order(request):
+    return render(request, 'urbanUmami/orders.html')
+
+def profile(request):
+    return render(request, 'urbanUmami/profile.html')
+
+def reserve(request):
+    return render(request, 'urbanUmami/reserve.html')
